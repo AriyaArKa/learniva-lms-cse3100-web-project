f18eec04de03ac55766f05fc859bd6217ad66e43
Arka Nath <natharka55@gmail.com>
2025-10-14 12:00:45 +0600

    fix:course updating in category card

--- lms/app/Models/Course.php
+++ lms/app/Models/Course.php
@@ -11,8 +11,14 @@ class Course extends Model
     use HasFactory;
     protected $guarded = [];
 
-    public function category(){
-        return $this->belongsTo(Category::class, 'category_id' ,'id');
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'instructor_id', 'id');
     }
 
 
