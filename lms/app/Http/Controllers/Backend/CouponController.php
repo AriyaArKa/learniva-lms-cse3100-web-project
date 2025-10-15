6c4f75e591c337017e6ed13e3fee826da44d96d9
Arka Nath <natharka55@gmail.com>
2025-10-15 09:32:09 +0600

    feat: add functionality to manage coupons with add and store methods, and create corresponding views

--- lms/app/Http/Controllers/Backend/CouponController.php
+++ lms/app/Http/Controllers/Backend/CouponController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Models\Coupon;
 use App\Models\SubCategory;
 use Intervention\Image\Facades\Image;
+use Carbon\Carbon;
+
+
 
 class CouponController extends Controller
 {
@@ -16,6 +19,30 @@ class CouponController extends Controller
         return view('admin.backend.coupon.coupon_all',compact('coupon'));
 
     } /// End Method 
+
+     public function AdminAddCoupon(){
+        return view('admin.backend.coupon.coupon_add');
+    }/// End Method 
+
+    public function AdminStoreCoupon(Request $request){
+        
+        Coupon::insert([
+            'coupon_name' => strtoupper($request->coupon_name),
+            'coupon_discount' => $request->coupon_discount,
+            'coupon_validity' => $request->coupon_validity,
+            'created_at' => Carbon::now(),
+
+        ]);
+
+        $notification = array(
+            'message' => 'Coupon Inserted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('admin.all.coupon')->with($notification);  
+
+
+    }/// End Method 
+    
 
 
 
--- lms/routes/web.php
+++ lms/routes/web.php
@@ -112,6 +112,9 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     // Admin Coupon All Route 
     Route::controller(CouponController::class)->group(function () {
         Route::get('/admin/all/coupon', 'AdminAllCoupon')->name('admin.all.coupon');
+        Route::get('/admin/add/coupon', 'AdminAddCoupon')->name('admin.add.coupon');
+        Route::post('/admin/store/coupon', 'AdminStoreCoupon')->name('admin.store.coupon');
+
 
 
     });
