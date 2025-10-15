f7de27f6d41b5db870f612c8d5976cd671f3032a
Arka Nath <natharka55@gmail.com>
2025-10-15 10:42:39 +0600

    feat: implement order confirmation email functionality with Orderconfirm Mailable and email template

--- lms/app/Http/Controllers/Frontend/CartController.php
+++ lms/app/Http/Controllers/Frontend/CartController.php
@@ -18,6 +18,8 @@ use App\Models\Coupon;
 use Illuminate\Support\Facades\Session;
 use App\Models\Payment;
 use App\Models\Order;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Orderconfirm;
 
 
 class CartController extends Controller
@@ -288,6 +290,23 @@ class CartController extends Controller
            } // end foreach 
 
            $request->session()->forget('cart');
+
+           $paymentId = $data->id;
+
+           /// Start Send email to student ///
+           $sendmail = Payment::find($paymentId);
+           $data = [
+                'invoice_no' => $sendmail->invoice_no,
+                'amount' => $total_amount,
+                'name' => $sendmail->name,
+                'email' => $sendmail->email,
+           ];
+
+           Mail::to($request->email)->send(new Orderconfirm($data));
+
+
+           /// End Send email to student /// 
+
 
             if ($request->cash_delivery == 'stripe') {
                echo "stripe";
