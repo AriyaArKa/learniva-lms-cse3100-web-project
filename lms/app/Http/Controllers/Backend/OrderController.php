8dbdc78cbf00571f9df0ed48ab6392d6a8f19f71
Arka Nath <natharka55@gmail.com>
2025-10-25 02:14:33 +0600

    feat: add invoice generation functionality for instructor orders with PDF support and update routes and views accordingly

--- lms/app/Http/Controllers/Backend/OrderController.php
+++ lms/app/Http/Controllers/Backend/OrderController.php
@@ -11,71 +11,101 @@ use App\Models\Course_goal;
 use App\Models\CourseSection;
 use App\Models\CourseLecture;
 use Intervention\Image\Facades\Image;
-use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use App\Models\Coupon;
 use Illuminate\Support\Facades\Session;
 use App\Models\Payment;
 use App\Models\Order;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+
 
 class OrderController extends Controller
 {
     //
-    public function AdminPendingOrder(){
+    public function AdminPendingOrder()
+    {
 
-        $payment = Payment::where('status','pending')->orderBy('id','DESC')->get();
-        return view('admin.backend.orders.pending_orders',compact('payment'));
+        $payment = Payment::where('status', 'pending')->orderBy('id', 'DESC')->get();
+        return view('admin.backend.orders.pending_orders', compact('payment'));
 
     } // End Method 
 
-     public function AdminOrderDetails($payment_id){
+    public function AdminOrderDetails($payment_id)
+    {
 
-        $payment = Payment::where('id',$payment_id)->first();
-        $orderItem = Order::where('payment_id',$payment_id)->orderBy('id','DESC')->get();
+        $payment = Payment::where('id', $payment_id)->first();
+        $orderItem = Order::where('payment_id', $payment_id)->orderBy('id', 'DESC')->get();
 
-        return view('admin.backend.orders.admin_order_details',compact('payment','orderItem'));
+        return view('admin.backend.orders.admin_order_details', compact('payment', 'orderItem'));
 
     }//end method
 
 
-    public function PendingToConfirm($payment_id){
+    public function PendingToConfirm($payment_id)
+    {
         Payment::find($payment_id)->update(['status' => 'confirm']);
 
         $notification = array(
             'message' => 'Order Confrim Successfully',
             'alert-type' => 'success'
         );
-        return redirect()->route('admin.confirm.order')->with($notification);  
+        return redirect()->route('admin.confirm.order')->with($notification);
 
 
 
     }// End Method 
 
 
-    public function AdminConfirmOrder(){
+    public function AdminConfirmOrder()
+    {
 
-        $payment = Payment::where('status','confirm')->orderBy('id','DESC')->get();
-        return view('admin.backend.orders.confirm_orders',compact('payment'));
+        $payment = Payment::where('status', 'confirm')->orderBy('id', 'DESC')->get();
+        return view('admin.backend.orders.confirm_orders', compact('payment'));
 
     } // End Method 
 
-     public function InstructorAllOrder(){
+    public function InstructorAllOrder()
+    {
 
         $id = Auth::user()->id;
-        $orderItem = Order::where('instructor_id',$id)->orderBy('id','desc')->get();
-        return view('instructor.orders.all_orders',compact('orderItem'));
+        $latestOrderItem = Order::where('instructor_id', $id)->select('payment_id', DB::raw('MAX(id) as max_id'))->groupBy('payment_id');
+
+        $orderItem = Order::joinSub($latestOrderItem, 'latest_order', function ($join) {
+            $join->on('orders.id', '=', 'latest_order.max_id');
+        })->orderBy('latest_order.max_id', 'DESC')->get();
+
+        return view('instructor.orders.all_orders', compact('orderItem'));
 
     }// End Method 
 
- public function InstructorOrderDetails($payment_id){
+    public function InstructorOrderDetails($payment_id)
+    {
 
-        $payment = Payment::where('id',$payment_id)->first();
-        $orderItem = Order::where('payment_id',$payment_id)->orderBy('id','DESC')->get();
+        $payment = Payment::where('id', $payment_id)->first();
+        $orderItem = Order::where('payment_id', $payment_id)->orderBy('id', 'DESC')->get();
 
-        return view('instructor.orders.instructor_order_details',compact('payment','orderItem'));
+        return view('instructor.orders.instructor_order_details', compact('payment', 'orderItem'));
 
     }// End Method
+
+
+    public function InstructorOrderInvoice($payment_id)
+    {
+
+        $payment = Payment::where('id', $payment_id)->first();
+        $orderItem = Order::where('payment_id', $payment_id)->orderBy('id', 'DESC')->get();
+
+        $pdf = Pdf::loadView('instructor.orders.order_pdf', compact('payment', 'orderItem'))->setPaper('a4')->setOption([
+            'tempDir' => public_path(),
+            'chroot' => public_path(),
+        ]);
+        return $pdf->download('invoice.pdf');
+
+    }// End Method 
 
 
 
